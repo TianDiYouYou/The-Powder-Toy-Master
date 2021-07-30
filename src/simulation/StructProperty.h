f6bc23d61ee3bcaf1a4e7693541c9e340a0f19b9
沈一村 <13167017708@163.com>
2021-07-30 16:13:04 +0800

    添加项目文件。

---
+++ src/simulation/StructProperty.h
@@ -0,0 +1,49 @@
+#ifndef STRUCTPROPERTY_H_
+#define STRUCTPROPERTY_H_
+
+#include "common/String.h"
+#include <cstdint>
+
+struct StructProperty
+{
+	enum PropertyType
+	{
+		TransitionType,
+		ParticleType,
+		Colour,
+		Integer,
+		UInteger,
+		Float,
+		BString,
+		String,
+		UChar,
+		Removed
+	};
+	ByteString Name;
+	PropertyType Type;
+	intptr_t Offset;
+
+	StructProperty(ByteString name, PropertyType type, intptr_t offset):
+	Name(name),
+	Type(type),
+	Offset(offset)
+	{
+
+	}
+
+	StructProperty():
+	Name(""),
+	Type(Integer),
+	Offset(0)
+	{
+
+	}
+};
+
+union PropertyValue {
+	int Integer;
+	unsigned int UInteger;
+	float Float;
+};
+
+#endif
